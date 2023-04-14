c44694ac989d4b6d0fa19a959c592f3dffd4abbf
eliezergyan <eliezer.gyan@amalitech.com>
2023-04-14 23:31:29 +0000

    Modified for Elvis' signals

--- run.py
+++ run.py
@@ -37,13 +37,80 @@ logger = logging.getLogger(__name__)
 CALCULATE, TRADE, DECISION = range(3)
 
 # allowed FX symbols
-SYMBOLS = ['GBPUSDm', 'EURUSDm', 'US30m', 'XAUUSDm','AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD', 'EURUSD', 'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPNZD', 'GBPUSD', 'NOW', 'NZDCAD', 'NZDCHF', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY', 'XAGUSD', 'XAUUSD']
+SYMBOLS = ['GBPUSDm', 'EURUSDm', 'US30m', 'XAUUSDm','AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD', 'EURUSD', 'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPNZD', 'GBPUSD', 'NOW', 'NZDCAD', 'NZDCHF', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY', 'XAGUSD', 'XAUUSD', 'US30']
 
 # RISK FACTOR
 RISK_FACTOR = float(os.environ.get("RISK_FACTOR"))
 
 
 # Helper Functions
+# def ParseSignal(signal: str) -> dict:
+#     """Starts process of parsing signal and entering trade on MetaTrader account.
+
+#     Arguments:
+#         signal: trading signal
+
+#     Returns:
+#         a dictionary that contains trade signal information
+#     """
+
+#     # converts message to list of strings for parsing
+#     signal = signal.splitlines()
+#     signal = [line.rstrip() for line in signal]
+
+#     trade = {}
+
+#     # determines the order type of the trade
+#     if('Buy Limit'.lower() in signal[0].lower()):
+#         trade['OrderType'] = 'Buy Limit'
+
+#     elif('Sell Limit'.lower() in signal[0].lower()):
+#         trade['OrderType'] = 'Sell Limit'
+
+#     elif('Buy Stop'.lower() in signal[0].lower()):
+#         trade['OrderType'] = 'Buy Stop'
+
+#     elif('Sell Stop'.lower() in signal[0].lower()):
+#         trade['OrderType'] = 'Sell Stop'
+
+#     elif('Buy'.lower() in signal[0].lower()):
+#         trade['OrderType'] = 'Buy'
+    
+#     elif('Sell'.lower() in signal[0].lower()):
+#         trade['OrderType'] = 'Sell'
+    
+#     # returns an empty dictionary if an invalid order type was given
+#     else:
+#         return {}
+
+#     # extracts symbol from trade signal
+#     trade['Symbol'] = (signal[0].split())[-1].upper()
+    
+#     # checks if the symbol is valid, if not, returns an empty dictionary
+#     if(trade['Symbol'] not in SYMBOLS):
+#         return {}
+    
+#     # checks wheter or not to convert entry to float because of market exectution option ("NOW")
+#     if(trade['OrderType'] == 'Buy' or trade['OrderType'] == 'Sell'):
+#         trade['Entry'] = (signal[1].split())[-1]
+    
+#     else:
+#         trade['Entry'] = float((signal[1].split())[-1])
+    
+#     trade['StopLoss'] = float((signal[2].split())[-1])
+#     trade['TP'] = [float((signal[3].split())[-1])]
+
+#     # checks if there's a fourth line and parses it for TP2
+#     if(len(signal) > 4):
+#         trade['TP'].append(float(signal[4].split()[-1]))
+    
+#     # adds risk factor to trade
+#     trade['RiskFactor'] = RISK_FACTOR
+
+#     return trade
+
+
+# Eliezer's ParseSignal Function
 def ParseSignal(signal: str) -> dict:
     """Starts process of parsing signal and entering trade on MetaTrader account.
 
@@ -53,13 +120,12 @@ def ParseSignal(signal: str) -> dict:
     Returns:
         a dictionary that contains trade signal information
     """
-
+    
     # converts message to list of strings for parsing
     signal = signal.splitlines()
     signal = [line.rstrip() for line in signal]
-
+    print(signal)
     trade = {}
-
     # determines the order type of the trade
     if('Buy Limit'.lower() in signal[0].lower()):
         trade['OrderType'] = 'Buy Limit'
@@ -78,32 +144,57 @@ def ParseSignal(signal: str) -> dict:
     
     elif('Sell'.lower() in signal[0].lower()):
         trade['OrderType'] = 'Sell'
+
+    elif('Buy'.lower() in signal[1].lower()):
+        trade['OrderType'] = 'Buy'
+
+    elif('Sell'.lower() in signal[1].lower()):
+        trade['OrderType'] = 'Sell'
+
+    elif('Buy'.lower() in signal[2].lower()):
+        trade['OrderType'] = 'Buy'
+
+    elif('Sell'.lower() in signal[2].lower()):
+        trade['OrderType'] = 'Sell'
     
     # returns an empty dictionary if an invalid order type was given
     else:
         return {}
-
-    # extracts symbol from trade signal
-    trade['Symbol'] = (signal[0].split())[-1].upper()
     
+    # extracts symbol from trade signal
+    if('US 30'.lower() in signal[0].lower()):
+        trade['Symbol'] = 'US30'
+    else:
+        trade['Symbol'] = (signal[0].split())[0].upper()[0:6]
     # checks if the symbol is valid, if not, returns an empty dictionary
     if(trade['Symbol'] not in SYMBOLS):
         return {}
-    
+
     # checks wheter or not to convert entry to float because of market exectution option ("NOW")
     if(trade['OrderType'] == 'Buy' or trade['OrderType'] == 'Sell'):
-        trade['Entry'] = (signal[1].split())[-1]
+        trade['Entry'] = 'NOW'
     
     else:
         trade['Entry'] = float((signal[1].split())[-1])
-    
-    trade['StopLoss'] = float((signal[2].split())[-1])
-    trade['TP'] = [float((signal[3].split())[-1])]
 
-    # checks if there's a fourth line and parses it for TP2
-    if(len(signal) > 4):
-        trade['TP'].append(float(signal[4].split()[-1]))
+    # cheks if it is an EA signal or Elvis' signal    
+    if('EA SIGNAL INPUT'.lower() in signal[1].lower()):
+        trade['StopLoss'] = float((signal[9].split())[-1])
+        trade['TP'] = [float((signal[4].split())[-1])]
     
+    # checks if there's a fourth line and parses it for TP2
+        if(len(signal) > 4):
+            trade['TP'].append(float(signal[5].split()[1]))
+            trade['TP'].append(float(signal[6].split()[1]))
+            trade['TP'].append(float(signal[7].split()[1]))
+    else:        
+        trade['StopLoss'] = float((signal[8].split())[1])
+        trade['TP'] = [float((signal[3].split())[1])]
+    # checks if there's a fourth line and parses it for TP2
+        if(len(signal) > 4):
+            trade['TP'].append(float(signal[4].split()[1]))
+            trade['TP'].append(float(signal[5].split()[1]))
+            trade['TP'].append(float(signal[6].split()[1]))
     # adds risk factor to trade
     trade['RiskFactor'] = RISK_FACTOR
 
